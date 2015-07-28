77267c8692e89ec4db51c4dfde138ee505a31712
Daulet Baimbetov <daucloudlab@gmail.com>
2015-07-28 12:22:14 +0600

    Онлайн Библиотека (version 04) Добавелены модели для Book и Genre

---
+++ src/java/kz/kokmardan/model/genre/Genre.java
@@ -0,0 +1,17 @@
+package kz.kokmardan.model.genre;
+
+public class Genre {
+    
+    private String name ;
+    
+    public Genre(){}
+    
+    public void setName(String name){
+        this.name = name ;
+    }
+    
+    public String getName(){
+        return this.name ;
+    }
+    
+}
---
+++ src/java/kz/kokmardan/model/genre/GenreList.java
@@ -0,0 +1,57 @@
+package kz.kokmardan.model.genre;
+
+
+import java.lang.invoke.MethodHandles;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import kz.kokmardan.db.Database;
+
+public class GenreList {
+    
+    private ArrayList<Genre> genreList = new ArrayList<Genre>() ;
+    
+    private ArrayList<Genre> getGenres(){
+        Statement stmt = null ;
+        ResultSet rs = null; 
+        Connection conn = null ;
+        
+        try{
+            conn = Database.getConnection() ;
+            stmt = conn.createStatement() ;
+            rs = stmt.executeQuery("select * from genre order by name") ;
+            while(rs.next()){
+                Genre genre = new Genre() ;
+                genre.setName(rs.getString("name"));
+                genreList.add(genre );
+            }
+        }catch(SQLException ex){
+            Logger.getLogger(GenreList.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try{
+                if(rs != null)
+                    rs.close();
+                if(stmt != null)
+                    stmt.close();
+                if(conn != null)
+                    conn.close() ;
+            }catch(SQLException ex){
+                Logger.getLogger(GenreList.class.getName()).log(Level.SEVERE, null, ex) ;
+            }
+        }
+        
+        return genreList ;
+    }
+    
+    public ArrayList<Genre> getGenreList(){
+        if(!genreList.isEmpty())
+            return genreList ;
+        else
+            return getGenres() ;
+    }
+    
+}
